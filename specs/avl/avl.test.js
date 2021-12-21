752a098fb79d6fd197c8181daf3131b7d9893b98
Richard See <richard.see@argos.co.uk>
2021-12-21 22:38:29 +1300

    AVL Tree

--- specs/avl/avl.test.js
+++ specs/avl/avl.test.js
@@ -22,16 +22,157 @@
 */
 
 class Tree {
-  // code goes here
+  constructor() {
+    this.root = null;
+  }
+
+  add(value) {
+    if (!this.root) {
+      this.root = new Node(value);
+    } else {
+      this.root.add(value);
+    }
+  }
+
+  toObject() {
+    return this.root;
+  }
 }
 
 class Node {
-  // code also goes here
+  constructor(value = null, left = null, right = null) {
+    this.value = value;
+    this.left = left;
+    this.right = right;
+    this.height = 1;
+  }
+
+  add(value) {
+    // Decide to go left or right
+    if (value < this.value) {
+      // Left
+      if (!this.left) {
+        this.left = new Node(value);
+      } else {
+        this.add(value);
+      }
+      
+      // Update height
+      if (!this.right || this.right.height < this.left.height) {
+        this.height = this.left.height + 1;
+      }
+    } else {
+      // Right
+      if (!this.right) {
+        this.right = new Node(value);
+      } else {
+        this.add(value);
+      }
+
+      // Update height
+      if (!this.left || this.right.height > this.left.height) {
+        this.height = this.right.height + 1;
+      }
+    }
+
+    // Balance the tree
+    this.balance();
+  }
+
+  balance() {
+    // Ask if this node is out of balance
+    const leftHeight = this.left ? this.left.height : 0;
+    const rightHeight = this.right ? this.right.height : 0;
+
+    // If out of balance, do I need to single or double rotate
+    // Diff is at least 2
+    if (leftHeight > rightHeight + 1) {
+      const leftRightHeight = this.left.right ? this.left.right.height : 0;
+      const leftLeftHeight = this.left.left ? this.left.left.height : 0;
+
+      // Do a right rotation first
+      // If double call rotate on child then on self
+      // Double rotation
+      if (leftRightHeight > leftLeftHeight ) {
+        this.left.rotateRR();
+      }
+
+      // If single call rotate on self
+      this.rotateLL();
+    } else if (rightHeight > leftHeight + 1) {
+      const rightRightHeight = this.right.right ? this.right.right.height : 0;
+      const rightLeftHeight = this.right.left ? this.right.left.height : 0;
+
+      // Do a left rotation first
+      // If double call rotate on child then on self
+      // Double rotation
+      if (rightLeftHeight > rightRightHeight) {
+        this.right.rotateLL();
+      }
+
+      // If single call rotate on self
+      this.rotateRR();
+    }
+  }
+
+  rotateRR() {
+    // Call if right child is heavy
+    // Right to the left
+    const valueBefore = this.value;
+    const leftBefore = this.left;
+
+    this.value = this.right.value;
+    this.left = this.right;
+    this.right = this.right.right;
+    this.left.right = this.left.left;
+    this.left.left = leftBefore;
+    this.left.value = valueBefore;
+
+    this.left.updateInNewLocation();
+    this.updateInNewLocation();
+  }
+
+  rotateLL() {
+    // Call if left child is heavy
+    // Left to the right
+    const valueBefore = this.value;
+    const rightBefore = this.right;
+
+    this.value = this.left.value;
+    this.right = this.left;
+    this.left = this.left.left;
+    this.right.left = this.right.right;
+    this.right.right = rightBefore;
+    this.right.value = valueBefore;
+
+    this.right.updateInNewLocation();
+    this.updateInNewLocation();
+  }
+
+  updateInNewLocation() {
+    // Calculate new height
+    
+    // No children
+    if (!this.right && !this.left) {
+      this.height = 1;
+    } 
+    // Left child has greater height
+    else if (
+      !this.right ||
+      (this.left && this.right.height < this.left.height)
+    ) {
+      this.height = this.left.height + 1;
+    }
+    // Right child has greater height
+    else {
+      this.height = this.right.height + 1;
+    }
+  }
 }
 
 // unit tests
 // do not modify the below code
-describe.skip("AVL Tree", function () {
+describe("AVL Tree", function () {
   test("creates a correct tree", () => {
     const nums = [3, 7, 4, 6, 5, 1, 10, 2, 9, 8];
     const tree = new Tree();
